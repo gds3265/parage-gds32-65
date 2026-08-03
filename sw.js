142c92937dfa518bf584bc183995474dee407b1e
gds3265 <elodie.jegou.gds65@reseaugds.com>
2026-08-03 12:47:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-const C='parage-v1-2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','style.css','app.js','manifest.json','assets/logo-gds.png','assets/logo.png','clients.json']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='parage-v1-3';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','style.css','app.js','manifest.json','clients.json']))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
